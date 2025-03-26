efaa51f57e167cfda8e745a0ed1db63e6d609a31
Serux <serchi93@hotmail.com>
2025-03-27 00:45:51 +0100

    Base for C# CLI

---
+++ cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        //Defining and linking cli options
+        var fileOption = new Option<string>("--file", "Path to json file") { IsRequired = true };
+        var urlOption = new Option<string>("--url", "Url to CVE API") { IsRequired = true };
+
+        var rootCommand = new RootCommand();
+        rootCommand.AddOption(fileOption);
+        rootCommand.AddOption(urlOption);
+
+
+
+        rootCommand.SetHandler(
+            (fileValue, urlValue) =>
+                {
+                    // Your application logic goes here
+                    Console.WriteLine($"File: {fileValue}");
+                    Console.WriteLine($"Url: {urlValue}");
+                },
+                fileOption,
+                urlOption
+        );
+
+        return rootCommand.Invoke(args);
+    }
+}
